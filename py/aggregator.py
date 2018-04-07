6935b1f5e49e30932f424e02276c0c183369d063
flopp <fpigorsch@gmail.com>
2018-04-07 16:22:36 +0200

    init

---
+++ py/aggregator.py
@@ -0,0 +1,138 @@
+# Copyright 2018 Florian Pigorsch. All rights reserved.
+#
+# Use of this source code is governed by a MIT-style
+# license that can be found in the LICENSE file.
+
+import datetime
+import jinja2
+import json
+import os
+import re
+import shutil
+from downloader import Downloader
+from imagescaler import ImageScaler
+from podcastparser_patched import parse as podcastparser_parse
+
+
+class Aggregator:
+    def __init__(self, podcasts_json, cache_dir, export_dir, templates_dir, base_url):
+        self._downloader = Downloader(4, "Lauf Podcast Aggregator, lauf-podcasts@flopp.net")
+        self._imagescaler = ImageScaler(4)
+        self._podcasts_json_file = podcasts_json
+        self._raw_data_dir = cache_dir
+        self._export_dir = export_dir
+        self._base_url = base_url
+        self._podcasts = []
+        self._jinja = jinja2.Environment(loader=jinja2.FileSystemLoader(templates_dir))
+        self._jinja.filters['timestamp2date'] = self.format_date
+        self._jinja.filters['timestamp2datetime'] = self.format_datetime
+        self._jinja.filters['formatseconds'] = self.format_seconds
+        self._jinja.globals['now'] = datetime.datetime.now().timestamp()
+        self._jinja.globals['base_url'] = base_url
+
+    def format_date(self, value):
+        d = datetime.datetime.fromtimestamp(value)
+        return d.strftime('%F')
+
+    def format_datetime(self, value):
+        d = datetime.datetime.fromtimestamp(value)
+        return d.strftime('%F %T')
+
+    def format_seconds(self, value):
+        d = datetime.timedelta(seconds=value)
+        return str(d)
+
+    def clear_cache(self):
+        if os.path.isdir(self.cache_dir):
+            shutil.rmtree(self.cache_dir)
+
+    def sync(self):
+        with open(self._podcasts_json_file, 'r') as f:
+            self._podcasts = json.load(f)
+        # filter podcasts without title or feed url
+        self._podcasts = [p for p in self._podcasts if ('title' in p) and ('feed' in p)]
+        for podcast in self._podcasts:
+            title = podcast['title']
+            feed_url = podcast['feed']
+            sanitized_title = self.sanitize(title)
+            podcast['sanitized_title'] = sanitized_title
+            dir = '{}/{}'.format(self._raw_data_dir, sanitized_title)
+            podcast['raw_dir'] = dir
+            feed_file = '{}/feed'.format(dir)
+            podcast['feed_file'] = feed_file
+            os.makedirs(dir, exist_ok=True)
+            self._downloader.add_job(feed_url, feed_file)
+        self._downloader.run()
+        # filter podcasts with non-existent feed file
+        self._podcasts = [p for p in self._podcasts if os.path.exists(p['feed_file'])]
+        for podcast in self._podcasts:
+            feed_url = podcast['feed']
+            feed_file = podcast['feed_file']
+            with open(feed_file, 'r') as f:
+                podcast['data'] = podcastparser_parse(feed_url, f)
+            # determine latest publish date
+            last_publish = None
+            for episode in podcast['data']['episodes']:
+                if not last_publish or episode['published'] > last_publish:
+                    last_publish = episode['published']
+            podcast['last_publish'] = last_publish
+            # format descriptions
+            podcast['data']['description_formatted'] = self.format_description(podcast['data']['description'])
+            for episode in podcast['data']['episodes']:
+                episode['description_formatted'] = self.format_description(episode['description'])
+            # determine cover image
+            cover_url = None
+            if 'cover_url' in podcast:
+                cover_url = podcast['cover_url']
+            if not cover_url and ('cover_url' in podcast['data']):
+                cover_url = podcast['data']['cover_url']
+            if cover_url:
+                dir = '{}/{}'.format(self._raw_data_dir, podcast['sanitized_title'])
+                cover_file = '{}/cover'.format(dir)
+                self._downloader.add_job(cover_url, cover_file)
+        self._downloader.run()
+
+    def format_description(self, description):
+        return '<br />'.join(description.split('\n'))
+
+    def export(self):
+        for podcast in self._podcasts:
+            self._imagescaler.add_job(
+                '{}/{}/cover'.format(self._raw_data_dir, podcast['sanitized_title']),
+                '{}/{}/cover.jpg'.format(self._export_dir, podcast['sanitized_title']),
+                512)
+        self._imagescaler.run()
+        self.export_info()
+        self.export_impressum()
+        self.export_index()
+        for podcast in self._podcasts:
+            self.export_podcast(podcast)
+        self._imagescaler.run()
+
+    def export_info(self):
+        os.makedirs(self._export_dir, exist_ok=True)
+        with open('{}/info.html'.format(self._export_dir), 'w') as f:
+            template = self._jinja.get_template('info.html')
+            f.write(template.render())
+
+    def export_impressum(self):
+        os.makedirs(self._export_dir, exist_ok=True)
+        with open('{}/impressum.html'.format(self._export_dir), 'w') as f:
+            template = self._jinja.get_template('impressum.html')
+            f.write(template.render())
+
+    def export_index(self):
+        os.makedirs(self._export_dir, exist_ok=True)
+        with open('{}/index.html'.format(self._export_dir), 'w') as f:
+            template = self._jinja.get_template('index.html')
+            f.write(template.render(podcasts=self._podcasts))
+
+    def export_podcast(self, podcast):
+        dir = '{}/{}'.format(self._export_dir, podcast['sanitized_title'])
+        os.makedirs(dir, exist_ok=True)
+        with open('{}/index.html'.format(dir), 'w') as f:
+            template = self._jinja.get_template('podcast.html')
+            f.write(template.render(podcast=podcast, episodes=podcast['data']['episodes']))
+
+    def sanitize(self, s):
+        return re.sub(r'\W+', '-', s).lower()
---
+++ py/main.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+# Copyright 2018 Florian Pigorsch. All rights reserved.
+#
+# Use of this source code is governed by a MIT-style
+# license that can be found in the LICENSE file.
+
+
+import argparse
+import os
+from aggregator import Aggregator
+
+
+def main():
+    args_parser = argparse.ArgumentParser()
+    args_parser.add_argument('--cache-dir', dest='cache_dir', metavar='DIR', type=str, default='../.cache')
+    args_parser.add_argument('--export-dir', dest='export_dir', metavar='DIR', type=str, default='../.export')
+    args_parser.add_argument('--templates-dir', dest='templates_dir', metavar='DIR', type=str, default='../.templates')
+    args_parser.add_argument('--base-url', dest='base_url', metavar='URL', type=str)
+    args_parser.add_argument('--podcasts-json', dest='podcasts_json', metavar='FILE', type=str, default='../podcasts.json')
+    args_parser.add_argument('--clear-cache', dest='clear_cache', action='store_true')
+    args = args_parser.parse_args()
+
+    a = Aggregator(
+        podcasts_json=args.podcasts_json,
+        cache_dir=args.cache_dir,
+        export_dir=args.export_dir,
+        templates_dir=args.templates_dir,
+        base_url=args.base_url if args.base_url else 'file://{}'.format(os.path.abspath(args.export_dir)))
+    if args.clear_cache:
+        a.clear_cache()
+    a.sync()
+    a.export()
+
+
+if __name__ == '__main__':
+    main()
